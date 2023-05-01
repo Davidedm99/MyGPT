e4a85af61944936319d3b4f633ae27f6bccaf849
Roscio <davidedimarco99@gmail.com>
2023-05-01 13:18:40 +0200

    first part finished problem with the API exposure

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("form");
+const input = document.getElementById("chat-input");
+const messages = document.getElementById("messages");
+const apiKey = "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";
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const message = input.value;
+    input.value = "";
+  
+    messages.innerHTML +=   `<div class="user-message">
+                                <i class="fa-solid fa-user fa-lg"></i> 
+                                <span>${message}</span>
+                             </div>`;
+  
+    // Use axios library to make a POST request to the OpenAI API
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        prompt: message,
+        model: "text-davinci-003",
+        temperature: 0,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+    const chatbotResponse = response.data.choices[0].text;
+  
+    messages.innerHTML +=   `<div class="bot-message">
+                                <i class="fa-solid fa-robot"></i>
+                                <span>${chatbotResponse}</span>
+                             </div>`;
+  });
